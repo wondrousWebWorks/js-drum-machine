a415d4bbb85b10e38d8b2ded06d97186cae8ef7c
Johann Alberts <alberts.johann@gmail.com>
2020-12-06 20:53:54 +0000

    Refactor logic into playSound function and call on key press

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,30 @@
-window.addEventListener('keydown', e => {
+const playSound = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   key.classList.add('pressed');
 
-  const removeTransition = (e) => {
-    if (e.propertyName !== 'transform') return;
-
-    e.target.classList.remove('pressed');
-  }
-
-  const keys = document.querySelectorAll('.key');
-
-  keys.forEach(key => {
-    key.addEventListener('transitionend', (e) => {
-      removeTransition(e);
-    });
-  });
-  
   if (!audio) {
     return;
   }
 
   audio.currentTime = 0;
   audio.play();
+}
+
+
+
+const removeTransition = (e) => {
+  if (e.propertyName !== 'transform') return;
+
+  e.target.classList.remove('pressed');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => {
+  key.addEventListener('transitionend', (e) => {
+    removeTransition(e);
+  });
 });
+  
+window.addEventListener('keydown', playSound);
