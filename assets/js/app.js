cac8fca30bb02fe8009d21c16020748831c60e7b
Johann Alberts <alberts.johann@gmail.com>
2020-12-06 20:47:15 +0000

    Add logic to add and remove key class on button press

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
 window.addEventListener('keydown', e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add('pressed');
+
+  const removeTransition = (e) => {
+    if (e.propertyName !== 'transform') return;
+
+    e.target.classList.remove('pressed');
+  }
+
+  const keys = document.querySelectorAll('.key');
+
+  keys.forEach(key => {
+    key.addEventListener('transitionend', (e) => {
+      removeTransition(e);
+    });
+  });
   
   if (!audio) {
     return;
@@ -7,6 +23,4 @@ window.addEventListener('keydown', e => {
 
   audio.currentTime = 0;
   audio.play();
-
-  console.log(audio);
 });
